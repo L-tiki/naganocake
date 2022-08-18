93203ddf8dd183b47b02c1b19fadfda652d06002
yoshidablue <108563352+yoshidablue@users.noreply.github.com>
2022-08-18 19:25:47 +0900

    Revert "Yoshida"

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
-  root :to =>"public/homes#top"
-  namespace :admin do
-    root "homes#top"
-  end
-  get "homes/about"=>"public/homes#about"
-
-  devise_for :admin,skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
