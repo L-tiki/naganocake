b9e6f765d4c5db9a931be4138cfc822d2a988509
hiro0822 <107989420+hiro0822@users.noreply.github.com>
2022-08-18 06:36:34 +0000

    a2

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :customers,skip:[:passwords],controllers:{
-    registrations: "public/registrations",
-    sessions:'public/sessions'
-  }
-
-
-  devise_for :admin,skip:[:registrations,:passwords],controllers:{
-    sessions:"admin/sessions"
-  }
+  root :to =>"public/homes#top"
+  get "homes/about"=>"public/homes#about"
 
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
@@ -17,10 +10,9 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
-  root :to =>"public/homes#top"
-  get "homes/about"=>"public/homes#about"
-  
+
+
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
