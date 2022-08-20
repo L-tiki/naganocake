63f3a8a210798eeafaaca84b375e4cc0091a3b85
hiro0822 <107989420+hiro0822@users.noreply.github.com>
2022-08-20 05:24:30 +0000

    商品新規投稿

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+
+
+  root :to =>"public/homes#top"
+  namespace :admin do
+    root "homes#top"
+  end
+  get "homes/about"=>"public/homes#about"
+
+namespace :admin do
+    resources :items,only:[:index,:new,:create,:show,:edit]
+  end
+
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
