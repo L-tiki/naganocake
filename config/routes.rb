2e929b711ac39efd02a5527ec5e8c77693442332
EC2 Default User <ec2-user@ip-172-31-14-122.ap-northeast-1.compute.internal>
2022-08-20 07:29:52 +0000

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,12 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
-
+  #resources :addresses, only: [:creste, :index, :destroy, :edit, :update]
+   get "addresses"=>"public/addresses#index"
+   post "addresses"=>"public/addresses#create"
+   get "addresses/:id" => "public/addresses#edit", as: "address"
+   patch "addresses/:id" => "public/addresses#update"
+   delete "addresses/:id" => "public/addresses#destroy"
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
