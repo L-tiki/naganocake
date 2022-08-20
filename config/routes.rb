a5520c2a7e53894825b5110f18c76890586640f9
EC2 Default User <ec2-user@ip-172-31-26-59.ap-northeast-1.compute.internal>
2022-08-20 07:25:24 +0000

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+
   root :to =>"public/homes#top"
+
   namespace :admin do
     root "homes#top"
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:show, :index, :edit, :update]
   end
+
+  get "customers" => "public/customers#show", as: "customer"
+  get "customers/:id/edit" => "public/customers#edit", as: "edit_customer"
+  patch "customers/:id" => "public/customers#update", as: "update_customer"
+  get "customers/:id/unsubscribe" => "public/customers#unsubscribe", as: "unsubscribe"
+  patch "customers/:id/withdraw" => "customers#withdraw", as: "withdraw"
+
   get "homes/about"=>"public/homes#about"
 
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
