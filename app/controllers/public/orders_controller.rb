720bd3dacbc015e4e540e6c0d690466da66df897
EC2 Default User <ec2-user@ip-172-31-21-178.ap-northeast-1.compute.internal>
2022-08-24 11:35:12 +0000

    y

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -28,17 +28,37 @@ class Public::OrdersController < ApplicationController
 		elsif params[:order][:address_number] == "3"
 		  @address = Address.new
 
-    else
+      @order.postal_code = @address.postal_code
+      @order.name = @address.name
+      @order.address = @address.address
+      @address.save
+      else
       render :new
     end
   end
 
   def index
-    @orders = current_customer.orders
   end
 
   def create
-
+　  order = Order.new(order_params)
+    cart_items = current_customer.cart_items
+    order.customer_id = current_customer.id
+    order.shipping_cost = 800
+    order.save
+    cart_items.each do |cart|
+      order_detail = OrderDetail.new
+      #cart変数からorder_detailに各カラムに代入
+      order_detail.item_id  = cart.item_id
+      order_detail.amount = cart.amount
+      order_detail.price = (cart.item.price*1.10).floor
+      order_detail.order_id = order.id
+      order_detail.save
+    end
+    #cartアイテムの削除
+    current_customer.cart_items.destroy_all
+    #カラムの代入
+    redirect_to public_orders_complete_path
   end
 
   def show
@@ -50,5 +70,5 @@ class Public::OrdersController < ApplicationController
   private
   def order_params
     params.require(:order).permit(:postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status, :payment_method)
-  end
+end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,29 +4,17 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
 
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-  #消費税を加えた商品価格
-  def add_tax_price
-    (self.price * 1.10).round
-=======
   def with_tax_price
     (price * 1.1).floor
->>>>>>> origin/develop
   end
 
   def taxin_price
     price*1.1
+  end
+
+  #消費税を加えた商品価格
+  def add_tax_price
+    (self.price * 1.10).round
   end
 
 end
