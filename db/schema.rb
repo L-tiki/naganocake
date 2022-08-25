cbb64bfbc38db3d1757b3475f1d3a17aea3cb8f2
hiro0822 <107989420+hiro0822@users.noreply.github.com>
2022-08-25 04:22:28 +0000

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_23_073613) do
+ActiveRecord::Schema.define(version: 2022_08_18_045222) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,11 +62,11 @@ ActiveRecord::Schema.define(version: 2022_08_23_073613) do
   end
 
   create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
     t.integer "customer_id", null: false
     t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "item_id", null: false
   end
 
   create_table "customers", force: :cascade do |t|
